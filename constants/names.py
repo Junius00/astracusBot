1b7e3c5d3aa6812873e4586d5543df55b35a7556
Junius Pun <juniuspun00@gmail.com>
2022-07-02 01:12:37 +0800

    additional templates and prelim points

--- constants/names.py
+++ constants/names.py
@@ -5,6 +5,7 @@ OG_THERON = 'OG4 THERON'
 
 KEY_R = 'resource'
 KEY_B = 'building'
+KEY_P = 'point'
 
 B_HOUSE = 'House'
 B_VILLAGE = 'Village'
@@ -14,6 +15,8 @@ R_WHEAT = 'Wheat'
 R_WATER = 'Water'
 R_MINERAL = 'Mineral'
 R_WOOD = 'Wood'
+
+P_FLAG_STEAL = 'Flags Stolen'
 
 C_BLACK = 'black'
 C_BLUE = 'blue'
--- constants/templates.py
+++ constants/templates.py
@@ -1,4 +1,4 @@
-from constants.names import B_HOUSE, B_VILLAGE, B_ROAD, R_WATER, R_WOOD, R_MINERAL, R_WHEAT
+from constants.names import B_HOUSE, B_VILLAGE, B_ROAD, P_FLAG_STEAL, R_WATER, R_WOOD, R_MINERAL, R_WHEAT
 
 B_TEMPLATE = {
     B_HOUSE: 0,
@@ -10,4 +10,7 @@ R_TEMPLATE = {
     R_WOOD: 0,
     R_MINERAL: 0,
     R_WHEAT: 0
+}
+P_TEMPLATE = {
+    P_FLAG_STEAL: 0
 }
