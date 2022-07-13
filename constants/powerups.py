bb48b85fac426db601118559c449d212f9531a05
nghochi123 <nghochi123@gmail.com>
2022-07-14 01:24:15 +0800

    Adding descriptions to powerups

--- constants/powerups.py
+++ constants/powerups.py
@@ -1,10 +1,10 @@
 from constants.names import KEY_PUP_ACTION, KEY_PUP_DESC, KEY_PUP_IS_INSTANT, KEY_PUP_QUANTITY
 from powerups.actions import barter_trade, randomizer_of_destiny, fate_of_hell, fools_luck, insurance, just_say_no, paving_the_way, power_of_trade, road_block, sneaky_thief, telescope
 
-#Price
+# Price
 PUP_RATIO = (15, [5])
 
-#Booster Cards
+# Booster Cards
 PUP_FOOLS_LUCK = "Fool's Luck"
 PUP_DICE_OF_DESTINY = "Dice of Destiny"
 PUP_POWER_OF_TRADE = "Power of Trade"
@@ -12,12 +12,13 @@ PUP_BARTER_TRADE = "Barter Trade"
 PUP_INSURANCE = "Insurance"
 PUP_PAVING_THE_WAY = "Paving the Way"
 
-#Action Cards
+# Action Cards
 PUP_ROAD_BLOCK = "Road Block"
 PUP_SNEAKY_THIEF = "Sneaky Thief"
 PUP_JUST_SAY_NO = "Just Say No"
 PUP_FATE_OF_HELL = "Fate of Hell"
 PUP_TELESCOPE = "Telescope"
+
 
 def PUP_INFO():
     return {
@@ -41,49 +42,49 @@ def PUP_INFO():
         },
         PUP_BARTER_TRADE: {
             KEY_PUP_QUANTITY: 4,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You can determine what resource to receive at any station game as the reward.',
             KEY_PUP_ACTION: barter_trade,
             KEY_PUP_IS_INSTANT: False
         },
         PUP_INSURANCE: {
             KEY_PUP_QUANTITY: 2,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You will be immune to the penalties imposed when other tribes steal your flag.',
             KEY_PUP_ACTION: insurance,
             KEY_PUP_IS_INSTANT: True
         },
         PUP_PAVING_THE_WAY: {
             KEY_PUP_QUANTITY: 5,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You can build a road for free.',
             KEY_PUP_ACTION: paving_the_way,
             KEY_PUP_IS_INSTANT: False
         },
         PUP_ROAD_BLOCK: {
             KEY_PUP_QUANTITY: 2,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You can reduce another tribe\'s earnings by 50 percent in the next game that they complete.',
             KEY_PUP_ACTION: road_block,
             KEY_PUP_IS_INSTANT: False
         },
         PUP_SNEAKY_THIEF: {
             KEY_PUP_QUANTITY: 3,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You can steal 25 random resources from another tribe of choice.',
             KEY_PUP_ACTION: sneaky_thief,
             KEY_PUP_IS_INSTANT: False
         },
         PUP_JUST_SAY_NO: {
             KEY_PUP_QUANTITY: 5,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You can now block another tribe’s action card.',
             KEY_PUP_ACTION: just_say_no,
             KEY_PUP_IS_INSTANT: True
         },
         PUP_FATE_OF_HELL: {
             KEY_PUP_QUANTITY: 3,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You can spin a roulette wheel to decide which opposing tribe to play the action against.',
             KEY_PUP_ACTION: fate_of_hell,
             KEY_PUP_IS_INSTANT: False
         },
         PUP_TELESCOPE: {
             KEY_PUP_QUANTITY: 2,
-            KEY_PUP_DESC: '',
+            KEY_PUP_DESC: 'You can know the identity and number of current points and resources of the leading tribe.',
             KEY_PUP_ACTION: telescope,
             KEY_PUP_IS_INSTANT: False
         }
