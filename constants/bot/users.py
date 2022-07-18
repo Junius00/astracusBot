ee231cd81c39c42bc44df2f69eb3caa5cc7dd378
Junius Pun <juniuspun00@gmail.com>
2022-07-18 17:10:02 +0800

    deployment ready

--- constants/bot/users.py
+++ constants/bot/users.py
@@ -5,15 +5,47 @@ ROLE_ADMIN = 'Admin'
 # OGs 1-4 use OG_AVARI, OG_KELGRAS, ...
 
 WHITELIST = {
-    'chknuggets': OG_AVARI,
-    'Yeast99': OG_AVARI,
-    'Soyabeaniess': OG_KELGRAS,
-    'meggolim': OG_LEVIATHAN,
-    'felicialeeee': ROLE_ADMIN,
-    'gohkaixuan': OG_THERON,
-    #
-    'fdddd75': OG_LEVIATHAN,
-    'cathsornnbot': ROLE_ADMIN,
-    'GravityGhost': OG_KELGRAS,
-    'juniuspun': ROLE_ADMIN,
+    # Main Comm
+    'gohkaixuan': ROLE_ADMIN, #chair
+    'juniuspun': ROLE_ADMIN, #vice-chair
+    'meggolim': ROLE_ADMIN, #CGL
+    'Soyabeaniess': ROLE_ADMIN, #Prog Head
+    'felicialeeee': ROLE_ADMIN, #Prog Head
+    'cathsornnbot': ROLE_ADMIN, #CSO
+    'Clajoannalim': ROLE_ADMIN, #Chief Pubs
+    'leahkxr': ROLE_ADMIN, #Chief Logs
+
+    #Proggies
+    'chknuggets': ROLE_ADMIN, #Ho Chi
+
+    #OG1 AVARI
+    'Yeast99': OG_AVARI, #Mai
+    'danishuzair': OG_AVARI, #Danish
+    'Tankyyyy': OG_AVARI, #Leng Ze
+    'leejiaqi': OG_AVARI, #Jia Qi
+    'onghaixiang': OG_AVARI, #Hai Xiang
+
+    #OG2 KELGRAS
+    'leongziwei': OG_KELGRAS, #Zi Wei
+    'nyanmao2': OG_KELGRAS, #Maw Htun
+    'GravityGhost': OG_KELGRAS, #Siyuan
+    'oyosuanchance': OG_KELGRAS, #Ian Lim
+    'joslyn_cwj': OG_KELGRAS, #Joslyn
+    'cherylcczq': OG_KELGRAS, #Cheryl Chua
+
+    #OG3 LEVIATHAN
+    'avelynnang': OG_LEVIATHAN, #Avelynn
+    'fdddd75': OG_LEVIATHAN, #Evan
+    'ntzeho': OG_LEVIATHAN, #Tze Ho
+    'Nathan_Teo': OG_LEVIATHAN, #Nathan
+    'augustinothia': OG_LEVIATHAN, #Albert
+    'Tchcheryl': OG_LEVIATHAN, #Cheryl Tan
+
+    #OG4 THERON
+    'j_ennnnnnnn': OG_THERON, #Jenn
+    'JinHong14': OG_THERON, #Jin Hong
+    'emnenms': OG_THERON, #Emily Yip
+    'ruiqingg': OG_THERON, #Rui Qing
+    'jovinshi': OG_THERON, #Jovin
+    'Ryan_Poon': OG_THERON, #Ryan Poon
 }
